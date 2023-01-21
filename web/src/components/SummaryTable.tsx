0d53b1eb3fe5398804d750b5b182ff6e25ca092b
Carlos Barros <carlos_dbs@hotmail.com>
2023-01-21 12:34:14 -0300

    feat(SummaryTable):fill dayHabits square in the grid

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,6 +1,12 @@
 import { HabitDay } from "./HabitDay";
 
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning";
+
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
+const summaryDates = generateDatesFromYearBeginning();
+
+const minimumSummaryDatesSize = 18 * 7 //18 weeks
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable(){
     return (
@@ -20,17 +26,19 @@ export function SummaryTable(){
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
-                <HabitDay />
+                {
+                    summaryDates.map((date) => {
+                        return < HabitDay key={ date.toString() } />
+                    })}
+
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
+                    return (
+                        <div
+                            key={ index }
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-700 rounded-lg opacity-40 cursor-not-allowed"
+                        />
+                    )
+                })}
             </div>
         </div>
     );
