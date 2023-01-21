69dd7ee8d8811fadba709300fd36684017dcf93d
Carlos Barros <carlos_dbs@hotmail.com>
2023-01-21 16:22:41 -0300

    feat:creates route to complete habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -75,4 +75,57 @@ export async function appRoutes(app: FastifyInstance) {
             completedHabits,
         }
     });
+
+    app.patch('/habits/:id/toggle', async (request) => {
+
+        const toggleHabitParams = z.object({
+            id: z.string().uuid()
+        });
+
+        const { id } = toggleHabitParams.parse(request.params);
+
+        const today = dayjs().startOf('day').toDate();
+
+        let day = await prisma.day.findUnique({
+            where: {
+                date: today,
+            }
+        });
+
+        if (!day) {
+            day = await prisma.day.create({
+                data: {
+                    date: today,
+                }
+            });
+        }
+
+        //Find completed habit
+        const dayHabit = await prisma.dayHabit.findUnique({
+            where: {
+                day_id_habit_id: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            }
+        });
+
+        if (dayHabit) {
+            await prisma.dayHabit.delete({
+                where: {
+                    id: dayHabit.id
+                }
+            })
+        } else {
+            //Complete Habit in current day
+            await prisma.dayHabit.create({
+                data: {
+                    day_id: day.id,
+                    habit_id: id
+                }
+            });
+        }
+    });
+
+
 }
